f77b18af97f2310ee66a78e4fe9127bcdfc0e7be
FlorFurch <florenciafurch@gmail.com>
2026-05-31 18:14:03 +0000

    Creación de App Aulas y Tablas

---
+++ fei2026tp/backend/aulas/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Carrera(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+
+    def __str__(self):
+        return self.nombre
+    
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+    apellido = models.CharField(max_length=128, null=False)
+    mostrar = models.CharField(max_length=256, null=False)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    
+class Materia(models.Model):
+    nombre = models.CharField(max_length=128, null=False)
+    cant_alumnos = models.IntegerField(default=5)
+    id_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    id_profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.id_carrera.nombre} - {self.id_profesor.nombre} {self.id_profesor.apellido}"
+
+class Aula(models.Model):
+    descripcion = models.CharField(max_length=128, null=False)
+    ubicacion = models.CharField(max_length=128, null=False)
+    cant_proyector = models.IntegerField(default=0)
+    aforo = models.IntegerField(default=0)
+    es_climatizada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.descripcion} - {self.ubicacion}"
